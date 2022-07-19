3695d7e034a6400b6fb2ca77a20bde72a8ba7b82
Romain <lemaire.romain37@gmail.com>
2022-07-19 16:36:15 +0200

    commit de l'espace

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {
-    path: 'auth',
+    path: '',
     loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)
   },
   {
