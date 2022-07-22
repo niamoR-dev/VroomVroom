b469cc585ab82440cce12b5411d44b330252ff2e
Romain <lemaire.romain37@gmail.com>
2022-07-22 16:21:16 +0200

    Commit du turfu

--- src/app/chef-atelier/chef-atelier.module.ts
+++ src/app/chef-atelier/chef-atelier.module.ts
@@ -8,7 +8,6 @@ import { ChefAtelierComponent } from "./components/chef-atelier.component";
 import { ListPieceComponent } from "../piece/components/list-piece/list-piece.component";
 import { EditPieceComponent } from "../piece/components/edit-piece/edit-piece.component";
 import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 import { AddPieceComponent } from "../piece/components/add-piece/add-piece.component";
 import { CreerClientComponent } from './components/creer-client/creer-client.component';
 import { TacheComponent } from '../tache/components/tache.component';
