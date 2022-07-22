06320276d0e907c87693c6afe6715e18b95497b2
Tibo Pfeifer <tibo.pfeifer@gmail.com>
2022-07-22 14:27:31 +0200

    (feat piece): add-piece working

--- src/app/piece/models/piece.ts
+++ src/app/piece/models/piece.ts
@@ -1,40 +1,15 @@
 import {Model} from "../../core/models/model";
 
 export class Piece extends Model {
-  private _dateSaisie!: Date;
-  private _libelle!: string;
-  private _quantite!: number;
-
+  public dateSaisie!: Date;
+  public libelle!: string;
+  public quantite!: number;
 
   constructor(dateSaisie: Date, libelle: string, quantite: number) {
     super();
-    this._dateSaisie = dateSaisie;
-    this._libelle = libelle;
-    this._quantite = quantite;
+    this.dateSaisie = dateSaisie;
+    this.libelle = libelle;
+    this.quantite = quantite;
   }
 
-
-  get dateSaisie(): Date {
-    return this._dateSaisie;
-  }
-
-  set dateSaisie(value: Date) {
-    this._dateSaisie = value;
-  }
-
-  get libelle(): string {
-    return this._libelle;
-  }
-
-  set libelle(value: string) {
-    this._libelle = value;
-  }
-
-  get quantite(): number {
-    return this._quantite;
-  }
-
-  set quantite(value: number) {
-    this._quantite = value;
-  }
 }
