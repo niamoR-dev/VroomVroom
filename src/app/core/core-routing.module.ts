025bd109631ca4df0304db61c97a4a2b26236a74
gmanchelin <gmanchelin@gmail.com>
2022-07-22 15:51:35 +0200

    Suppression Commercial
    Suppression du component Commercial, déplacement du vehicule.ts dans le Chef d'Atelier

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,16 +1,11 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
   {
     path: '',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'commercial',
-    loadChildren: () => import('../commercial/commercial.module')
-      .then(m => m.CommercialModule)
   },
   {
     path: 'admin',
