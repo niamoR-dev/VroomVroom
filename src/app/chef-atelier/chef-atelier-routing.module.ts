044c1d576b7960157bdca5ee4028bad89cef1449
Romain <lemaire.romain37@gmail.com>
2022-07-22 16:13:53 +0200

    sauvetage presque réuss

--- src/app/chef-atelier/chef-atelier-routing.module.ts
+++ src/app/chef-atelier/chef-atelier-routing.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { ChefAtelierComponent } from "./components/chef-atelier.component";
 import { FicheEntretienComponent } from "../fiche-entretien/components/list-fiche-entretien/fiche-entretien.component";
 import { ListPieceComponent } from "../piece/components/list-piece/list-piece.component";
 import { EditPieceComponent } from "../piece/components/edit-piece/edit-piece.component";
 import { AddPieceComponent } from "../piece/components/add-piece/add-piece.component";
-=======
-import {ListPieceComponent} from "../piece/components/list-piece/list-piece.component";
-import {ChefAtelierComponent} from "./components/chef-atelier.component";
-import {FicheEntretienComponent} from "../fiche-entretien/components/list-fiche-entretien/fiche-entretien.component";
-import {EditPieceComponent} from "../piece/components/edit-piece/edit-piece.component";
-import {AddPieceComponent} from "../piece/components/add-piece/add-piece.component";
 import { CreerClientComponent } from './components/creer-client/creer-client.component';
 import { TacheComponent } from '../tache/components/tache.component';
->>>>>>> GMA-page_create_client
+
 
 const routes: Routes = [
   {
