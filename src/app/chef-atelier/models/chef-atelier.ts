b7c99084c0e03966b0e2ab83a244b537e4532459
Romain <lemaire.romain37@gmail.com>
2022-07-26 15:40:16 +0200

    Commit 22

--- src/app/chef-atelier/models/chef-atelier.ts
+++ src/app/chef-atelier/models/chef-atelier.ts
@@ -1,7 +1,10 @@
-import {Utilisateur} from "../../core/models/utilisateur";
+import { Utilisateur } from "../../core/models/utilisateur";
 
 export class ChefAtelier extends Utilisateur {
-  override set idProfil(value: number) {
-    super.idProfil = 5;
+
+  constructor() {
+    super();
+    this.idProfil = 5;
   }
+
 }
--- src/app/fiche-entretien/models/jointureFicheClient.ts
+++ src/app/fiche-entretien/models/jointureFicheClient.ts
@@ -1,21 +1,15 @@
-import {Fiche} from "./fiche";
+import { Fiche } from "./fiche";
 
 export class JointureFicheClient extends Fiche {
 
-  private _client!: [];
+  client!: [];
 
 
   constructor(idTache: number[], idUtilisateur: number, clientId: number, description: string, dateCreation: Date, dateCloture: Date, ficheTerminee: boolean, montantTtc: number, client: []) {
     super(idTache, idUtilisateur, clientId, description, dateCreation, dateCloture, ficheTerminee, montantTtc);
-    this._client = client;
+    this.client = client;
   }
 
 
-  get client(): [] {
-    return this._client;
-  }
 
-  set client(value: []) {
-    this._client = value;
-  }
 }
