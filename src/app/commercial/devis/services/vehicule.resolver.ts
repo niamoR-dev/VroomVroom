b5250ec75834c26f0f0f4e50c323ee073e9195f5
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2022-07-28 10:42:36 +0200

    (feat commercial): CRUD devis in progress

---
+++ src/app/commercial/devis/services/vehicule.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import {ApiWebService} from "../../../shared/web-services/api.web-service";
+import {URL_LIST} from "../../../shared/utils/url.list";
+import {Vehicule} from "../../../shared/models/vehicule";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiculeResolver implements Resolve<any> {
+  constructor(private service: ApiWebService<Vehicule>) {
+
+  }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.service.getAllData(URL_LIST.vehicule)
+  }
+}
