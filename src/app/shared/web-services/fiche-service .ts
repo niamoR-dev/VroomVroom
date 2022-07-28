fe1ef8b8680929f487a28e1b07abd374615e374e
Romain <lemaire.romain37@gmail.com>
2022-07-28 10:34:02 +0200

    commit

--- src/app/shared/web-services/fiche-service .ts
+++ src/app/shared/web-services/fiche-service .ts
@@ -1,7 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../../../environments/environment";
-import {Model} from "../../core/models/model";
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Client } from '../models/client';
+import { Vehicule } from 'src/app/chef-atelier/models/vehicule';
+import { Utilisateur } from 'src/app/core/models/utilisateur';
 
 @Injectable({
   providedIn: 'root'
@@ -9,61 +10,19 @@ import {Model} from "../../core/models/model";
 /**
  * Main service for API calls for different objects
  */
-export class ApiWebService<T extends Model> {
+export class APIFicheComplete {
+
+  client!: Client;
+  vehicule!: Vehicule;
+  utilsateur!: Utilisateur;
+
 
   constructor(private http: HttpClient) {
   }
 
-  /**
-   * Gets data from all abjects from given type and stocks it in an array
-   * @param urlPart
-   */
-  getAllData(urlPart: string) {
-    return this.http.get<T[]>(`${environment.gateway + urlPart}`);
-  }
 
-  /**
-   * Get data from specified object of given type with ID
-   * @param id
-   * @param urlPart
-   */
-  getData(id: number, urlPart: string) {
-    return this.http.get<T>(`${environment.gateway + urlPart + id}`)
-  }
 
-  /**
-   * Adds a new object of given type
-   * @param p
-   * @param urlPart
-   */
-  addData(p: T, urlPart: string) {
-    p.id = 0;
-    return this.http.post(`${environment.gateway + urlPart}`, p)
-  }
+  // a partir d'une fiche, si elle existe, de la charger, d'aller chercher toutes les info fiches (ID)
+  // URL embed à aprtir de la fiche
 
-  /**
-   * Update data of specified object of given type with ID
-   * @param p
-   * @param urlPart
-   */
-  updateData(p: T, urlPart: string) {
-    this.http.put<T>(`${environment.gateway + urlPart + p.id}`, p).subscribe({
-      next: () => this.getAllData(urlPart),
-      error: err => console.log(`Error while updating data (${urlPart}): ` + err),
-      complete: () => console.log(`Update data complete (${urlPart})`)
-    })
-  }
-
-  /**
-   * Removes specified object of given type with ID
-   * @param id
-   * @param urlPart
-   */
-  deleteData(id: number, urlPart: string): void {
-    this.http.delete<T>(`${environment.gateway + urlPart + id}`).subscribe({
-      next: () => this.getAllData(urlPart),
-      error: err => console.log(`Error while removing data (${urlPart}): ` + err),
-      complete: () => console.log(`Removing data complete (${urlPart})`)
-    })
-  }
 }
